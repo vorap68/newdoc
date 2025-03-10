831a5da3e8112714cc25a4e699078c19ae2821fd
vorap68 <dmkaspar68@gmail.com>
2025-03-10 14:22:36 +0200

    Add_Vite_Into_Docker-compose

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,29 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+   
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  
+  //   host: true,
+  //   strictPort: true,
+  //   port: 5173,
+
+  //   hmr: {
+  //     host: 'localhost', // или IP машины хоста, если Vite должен быть доступен из контейнера nginx
+  //   },
+  // },
+
+  server: {
+    host: '0.0.0.0', // Чтобы Vite слушал все IP внутри Docker
+    port: 5173,
+    hmr: {
+        host: 'localhost', // или твой IP хоста, если работаешь не на localhost
+    },
+    cors: true, // включаем CORS
+},
+
+
 });
